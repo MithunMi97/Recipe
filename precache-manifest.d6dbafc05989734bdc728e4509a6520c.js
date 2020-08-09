286de9a8b95c1e46ee2d3f715427e369b8c305fc
mithun <49356216+MithunMi97@users.noreply.github.com>
2020-08-09 17:39:11 +0530

    Updates

---
+++ precache-manifest.d6dbafc05989734bdc728e4509a6520c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a561c97d2911346fda9c026d3c29f23",
+    "url": "/Recipe/index.html"
+  },
+  {
+    "revision": "31e1d78f8d30403fb232",
+    "url": "/Recipe/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "6bcae67e2b2b34c33c73",
+    "url": "/Recipe/static/css/main.337a24b9.chunk.css"
+  },
+  {
+    "revision": "31e1d78f8d30403fb232",
+    "url": "/Recipe/static/js/2.207f5bbe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Recipe/static/js/2.207f5bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bcae67e2b2b34c33c73",
+    "url": "/Recipe/static/js/main.910d5545.chunk.js"
+  },
+  {
+    "revision": "8b604af6e89a09682fbd",
+    "url": "/Recipe/static/js/runtime-main.2c8fc8d9.js"
+  },
+  {
+    "revision": "32b67c728ad8b691637e6832b8b11cef",
+    "url": "/Recipe/static/media/Img2.32b67c72.jpg"
+  },
+  {
+    "revision": "f7b5e589f88206b4bd5cb1408c5362e6",
+    "url": "/Recipe/static/media/Metropolis-Regular.f7b5e589.otf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Recipe/static/media/logo.5d5d9eef.svg"
+  }
+]);
